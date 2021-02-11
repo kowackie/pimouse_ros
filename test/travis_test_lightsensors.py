0c6d81bcf2b3708193d052fdfb8b155cd22eede1
kowackie <kowaki50@gmail.com>
2021-02-11 10:08:27 +0000

    Fix a bug

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -34,7 +34,7 @@ class LightsensorsTest(unittest.TestCase):
     def test_get_value(self):
         rospy.set_param('lightsensors_freq', 10)
         time.sleep(2)
-        with open('/dev/rtlightsensors0', 'w') as f:
+        with open('/dev/rtlightsensor0', 'w') as f:
             f.write("-1 0 123 4321\n")
         time.sleep(3)
         self.assertFalse(self.count == 0, "cannot subscribe the topic")
@@ -45,8 +45,8 @@ class LightsensorsTest(unittest.TestCase):
         time.sleep(2)
         c_prev = self.count
         time.sleep(3)
-        self.assertTrue(self.count < c_freq + 4, "freq does not change")
-        self.assertFalse(self.count == c_freq, "subscriber is stopped")
+        self.assertTrue(self.count < f_prev + 4, "freq does not change")
+        self.assertFalse(self.count == f_prev, "subscriber is stopped")
 
 if __name__ == '__main__':
     time.sleep(3)
